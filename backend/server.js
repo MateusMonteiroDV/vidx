b18257b16fe3d94aa280daca01e7f5034917c9de
Mateus <mateusdosantosmonteiro12@gmail.com>
2025-02-13 23:00:05 -0300

    Adding backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json())
+
+
+
+
+
+
+const PORT = process.env.SERVER_PORT || 5000
+
+
+app.listen(PORT,()=>{
+	console.log('server running on port' + PORT)
+
+})
+
+
+
+
+
+
